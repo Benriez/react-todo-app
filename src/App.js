445bd6054ebba6d97add4747417ed9fba4adf1c2
Benriez <33022072+Benriez@users.noreply.github.com>
2020-06-20 20:06:23 +0200

    added index.css and app.css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, {useState} from 'react';
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
+import './App.css';
 
 function App() {
   const [todos, setTodos] = useState ([]);
   
+  function addTodo(todo) {
+    setTodos([todo, ...todos]);
+  }
+
   return (
     <div className="App">
      <header className="App-header">
         <p>React Todo</p>
+        <TodoForm addTodo={addTodo} />
+        <TodoList todos={todos} />
      </header>
     </div>
   );
