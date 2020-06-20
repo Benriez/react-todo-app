8dc17d5aa81c30dc137e2ccc4ccf1acb730e17fd
Benriez <33022072+Benriez@users.noreply.github.com>
2020-06-20 20:34:01 +0200

    added removeTodo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
 import './App.css';
 
+const LOCAL_STORAGE_KEY = "react-todo-list-todos";
+
 function App() {
   const [todos, setTodos] = useState ([]);
+
+  useEffect(() => {
+    const storageTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (storageTodos) {
+      setTodos(storageTodos);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+  }, [todos]);
   
   function addTodo(todo) {
     setTodos([todo, ...todos]);
+  }
+
+  function toggleComplete(id) {
+    setTodos(
+      todos.map(todo => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed
+          };
+        }
+        return todo;
+      })
+    );
+  }
+
+  function removeTodo(id){
+    setTodos(todos.filter(todo => todo.id !== id));
   }
 
   return (
@@ -15,7 +46,11 @@ function App() {
      <header className="App-header">
         <p>React Todo</p>
         <TodoForm addTodo={addTodo} />
-        <TodoList todos={todos} />
+        <TodoList 
+          todos={todos} 
+          toggleComplete={toggleComplete}
+          removeTodo={removeTodo}  
+        />
      </header>
     </div>
   );
