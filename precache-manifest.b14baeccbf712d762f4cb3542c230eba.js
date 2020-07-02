4e66a518c8472ef713fc1439354ebfbace11cec1
Benriez <stberiez@stud.h-da.de>
2020-07-02 15:05:15 +0200

    Updates

---
+++ precache-manifest.b14baeccbf712d762f4cb3542c230eba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75151365814880c245750178fee4d17a",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "269098ff2e9e2aaa2d54",
+    "url": "/react-todo-app/static/css/main.d30aa43d.chunk.css"
+  },
+  {
+    "revision": "51b83a65900f302eac9e",
+    "url": "/react-todo-app/static/js/2.c7843ea9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-todo-app/static/js/2.c7843ea9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "269098ff2e9e2aaa2d54",
+    "url": "/react-todo-app/static/js/main.ae787ded.chunk.js"
+  },
+  {
+    "revision": "496610ea5e3a283156cb",
+    "url": "/react-todo-app/static/js/runtime-main.0250d4e1.js"
+  }
+]);
